04b27782df086b007afff770d28ee7e23f145dd5
veeresh-code24 <veereshkanamadi@gmail.com>
2026-04-22 19:46:39 +0530

    small work

--- two_pointer_sliding_window.py/longest_substring.py
+++ two_pointer_sliding_window.py/longest_substring.py
@@ -1,20 +1,75 @@
-def longest_substring(s):
+# Brute force
+
+'''def longest_substring(s):
     n = len(s)
-    j = 0
+    max_len = 0
 
-    for i in range(1,n):
-        if s[i] == s[j]:
-            return i
+    for i in range(n):
+        visited = [0]*256
+        for j in range(i,n):
+            if visited[ord(s[j])] == 1:
+                break
 
+            visited[ord(s[j])] = 1
 
+            max_len = max(max_len,j-i+1)
 
+    return max_len
 
-
-
-
-
-
-# s = "abcabcbb"
+s = "abcabcbb"
 s = "bbbbbb"
 s = "pwwkew"
+s = "cadbzabcd"
+print(longest_substring(s))'''
+
+# Brute force
+
+'''def longest_substring(s):
+    n = len(s)
+    max_len = 0
+
+    for i in range(n):
+        hashset = set()
+        for j in range(i,n):
+            if s[j] in hashset:
+                break
+            hashset.add(s[j])
+
+            max_len = max(max_len, j-i+1)
+
+    return max_len
+
+s = "abcabcbb"
+s = "bbbbbb"
+s = "pwwkew"
+s = "cadbzabcd"
+print(longest_substring(s))'''
+
+# Optimization
+
+def longest_substring(s):
+    n = len(s)
+    left,right,max_len = 0,0,0
+    hashset = [-1]*256
+
+    while right < n:
+        if hashset[ord(s[right])] != -1:
+            left = max(hashset[ord(s[right])]+1,left)
+
+        max_len = max(right-left+1,max_len)
+
+        hashset[ord(s[right])] = right
+        right += 1
+
+    return max_len
+
+
+
+
+
+
+s = "abcabcbb"
+# s = "bbbbbb"
+# s = "pwwkew"
+# s = "cadbzabcd"
 print(longest_substring(s))
