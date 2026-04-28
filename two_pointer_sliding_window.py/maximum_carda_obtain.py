19898c67feafda0123a53551147c2811b0648eb5
veeresh-code24 <veereshkanamadi@gmail.com>
2026-04-28 22:44:42 +0530

    small work

--- two_pointer_sliding_window.py/maximum_carda_obtain.py
+++ two_pointer_sliding_window.py/maximum_carda_obtain.py
@@ -1,25 +1,25 @@
-def maximum_cards(cards,k):
-    n = len(cards)
+def maximum_cards(cardspoints,k):
+    n = len(cardspoints)
     max_len = 0
 
     for i in range(k+1):
         temp_sum = 0
-
         for j in range(i):
-            temp_sum += cards[j]
+            temp_sum += cardspoints[j]
 
         for j in range(k-i):
-            temp_sum += cards[n-1-j]
+            temp_sum += cardspoints[n-1-j]
+
 
         max_len = max(max_len,temp_sum)
 
     return max_len
 
-# cards = [1,2,3,4,5,6,1]
+# cardspoints = [1,2,3,4,5,6,1]
 # k = 3
 # cards = [2,2,2]
-cards = [9,7,7,9,7,7,9]
+cardspoints = [9,7,7,9,7,7,9]
 k = 7
 
 # k = 2
-print(maximum_cards(cards,k))
+print(maximum_cards(cardspoints,k))
