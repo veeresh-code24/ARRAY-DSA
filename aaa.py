a03e0049eaa67d8df758cad72da23388a12846a6
veeresh-code24 <veereshkanamadi@gmail.com>
2026-05-15 21:54:44 +0530

    small work

--- aaa.py
+++ aaa.py
@@ -16,7 +16,205 @@ def binary_search(nums,target):
 
     return -1
 
-    
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
+nums = [-1,0,3,5,9,12]
+target = 9
+print(binary_search(nums,target))
+
+def binary_search(nums,target):
+
+    low,high = 0, len(nums)-1
+
+    while low <= high:
+        mid = (low+high)//2
+
+        if nums[mid] == target:
+            return mid
+        
+        elif nums[mid] < target:
+            low = mid+1
+
+        else:
+            high = mid-1
+
+    return -1
+
 nums = [-1,0,3,5,9,12]
 target = 9
 print(binary_search(nums,target))
