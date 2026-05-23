d9aeda2a786312295fbb34934a8d2d5fb9e5c86e
veeresh-code24 <veereshkanamadi@gmail.com>
2026-05-24 00:52:19 +0530

    revision work

--- aaa.py
+++ aaa.py
@@ -3,37 +3,61 @@ class Node:
         self.data = data
         self.next = None
 
-def TraverseArray(arr):
+
+#  Traversing Array to LL
+def ConvertArrayLL(arr):
     head = Node(arr[0])
     movers = head
 
     for i in range(1,len(arr)):
         temp = Node(arr[i])
         movers.next = temp
+
         movers = temp
 
     return head
 
-def PrintLL(head):
+# length of LL
+def LengthOfArray(head):
+    count = 0
     temp = head
 
     while temp != None:
-        print(temp.data, end=" ")
+        count += 1
 
         temp = temp.next
 
-    # print()
+    return count
 
+# Checking present Value
+def CheckIfPresent(head,val):
+    temp = head
+
+    while temp != None:
+       if temp.data == val:
+           return 1
+       
+       temp = temp.next
+       
+    return 0
 
 def main():
-    arr = [2,5,8,6]
 
-    head = TraverseArray(arr)
-    print(head.data)
+    arr = [2,4,6,8,9]
+    head = ConvertArrayLL(arr)
+    # print(head.data)
+    # print(LengthOfArray(head))
+    # print(length)
+    present = CheckIfPresent(head,9)
+    print(present)
 
-    head = PrintLL(head)
+    temp = head
+    # Printing LL
+    while temp != None:
+        print(temp.data,end = " ")
+        temp = temp.next
+
+    print()
 
 if __name__ == '__main__':
     main()
-
-
