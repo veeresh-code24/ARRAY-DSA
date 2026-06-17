865a3e8996ef6541453443fb4b63d5d6329e3f10
veeresh-code24 <veereshkanamadi@gmail.com>
2026-06-17 23:46:18 +0530

    inital work

--- binary_search.py/Linked_List.py/12_intersection_LL.py
+++ binary_search.py/Linked_List.py/12_intersection_LL.py
@@ -1,35 +1,52 @@
-# Brute Force
-
 class Node:
-    def __init__(self,data,next=None):
+    def __init__(self, data):
         self.data = data
-        self.next = next
+        self.next = None
 
 
 class Solution:
-    def IntersectionLL(self,head):
+    def IntersectionLL(self, headA, headB):
 
-                    
+        tempA = headA
 
-if __name__ == '__main__':
-    head = Node(1)
-    second = Node(2)
-    third = Node(3)
-    fourth = Node(4)
-    fifth = Node(5)
+        while tempA:
+            tempB = headB
 
-    head.next = second
-    second.next = third
-    third.next = fourth
-    fourth.next = fifth
+            while tempB:
+                if tempA == tempB:  # Same node address
+                    return tempA
 
-    fifth.next = second
-    # third.next = second
+                tempB = tempB.next
 
-sol = Solution()
-startNode = sol.IntersectionLL(head)
+            tempA = tempA.next
 
-if startNode:
-    print("Loop is present in linkedList",startNode)
-else:
-    print("Loop is not in linkedList")
+        return None
+
+
+if __name__ == "__main__":
+
+    # Common part
+    common1 = Node(8)
+    common2 = Node(10)
+
+    common1.next = common2
+
+    # First Linked List: 1 -> 2 -> 8 -> 10
+    headA = Node(1)
+    secondA = Node(2)
+
+    headA.next = secondA
+    secondA.next = common1
+
+    # Second Linked List: 3 -> 8 -> 10
+    headB = Node(3)
+
+    headB.next = common1
+
+    sol = Solution()
+    intersectionNode = sol.IntersectionLL(headA, headB)
+
+    if intersectionNode:
+        print("Intersection Node:", intersectionNode.data)
+    else:
+        print("No Intersection")
