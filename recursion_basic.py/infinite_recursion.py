c0bc4fcce0a9c8652438c0043fcc4c49893f2b58
veeresh-code24 <veereshkanamadi@gmail.com>
2026-07-15 11:11:55 +0530

    initial work

--- recursion_basic.py/infinite_recursion.py
+++ recursion_basic.py/infinite_recursion.py
@@ -9,145 +9,136 @@ def fun():
 
 fun()'''
 
-# Base condition
+# INFINITE Recursion
 
-'''def fun(cnt):
+'''def f():
+    print(1)
+    f()
 
-    if cnt == 3:
+def main():
+    f()
+
+main()'''
+
+
+# With a Base Condition
+
+'''cnt = 0
+def f():
+    global cnt
+    if (cnt==3):
         return 
     
-
     print(cnt)
     cnt += 1
 
-    fun(cnt)
-
-
+    f()
 def main():
-    fun(0)
+    f()
+
+main()'''
+
+'''def f(cnt):
+    if cnt == 10:
+        return
+    print(cnt)
+
+    f(cnt + 1)
 
 
-if __name__ == '__main__':
-    main()'''
+f(0)'''
 
-# Print Name N times
+# Print name 5 times
 
-'''def fun(i,n):
-
-    if i == n:
+'''def f(name):
+    if name == 5:
         return 
     
     print("Iranna")
-    fun(i+1,n)
+    f(name + 1)
 
-n = 5
-fun(0,n)'''
+f(0)'''
 
-# Print Linearly from 1 to N
 
-'''def fun(i,n):
+# Print N Times
 
+'''def f(i,n):
+    if (i > n):
+        return 
+    
+    print("Iranna")
+    f(i+1, n)
+
+
+n = int(input("enter a number"))
+f(1,n)'''
+
+# Print 1 to N
+
+'''def f(i,n):
     if i > n:
         return 
     
     print(i)
-    fun(i+1,n)
-
-
-n = 4
-fun(1,n)'''
-
-# Print in terms N to 1
-
-'''def fun(i,n):
-    if i < 1:
-        return
-    print(i)
-    fun(i-1,n) 
+    f(i+1,n)
 
 def main():
-    n = int(input("Rnter the number:"))
-    fun(n,n)
+    n = int(input("Enter the numbers"))
+    f(1,n)
 
-if __name__ == '__main__':
-    main()'''
+main()'''
 
-# Print Linearly from 1 to N (But By Backtracking)
+# Print N to 1
 
-'''def fun(i,n):
+'''def f(i,n):
     if i < 1:
         return
     
-    fun(i-1,n)
     print(i)
-
+    f(i-1, n)
 
 def main():
     n = int(input("Enter the number: "))
-    fun(n,n)
+    f(10,n)
 
-if __name__ == '__main__':
-    main()'''
+main()'''
 
-# Print Linearly from N to 1 (But By Backtracking)
+# BACKTRACKING N to 1
 
-'''def fun(i,n):
+'''def f(i,n):
     if i > n:
         return 
     
-    fun(i+1,n)
+    f(i+1,n)
+    print(i)
+
+def main():
+    n = int(input("Enter the number: "))
+    f(1,n)
+
+main()'''
+
+# BACKTRACKING 1 TO N
+
+def f(i,n):
+    if i < 1:
+        return 
+    
+    f(i-1,n)
     print(i)
 
 
 def main():
-    n = int(input("Enter the number: "))
-    fun(1,n)
+    n = int(input("Enter the Number: "))
+    f(n,n)
 
-if __name__ == "__main__":
-    main()'''
-
-# Sum Of First N numbers ---paramaterzied recursion---
-# Work happens while going down
-
-'''def fun(i,sum):
-    if i < 1:
-        print(sum)
-        return
-    
-    fun(i-1,sum+i)
-
-def main():
-    n = int(input("Enter the number: "))
-    fun(n,0)
-
-if __name__ == '__main__':
-    main()'''
-
-# Functional Recursion
-# Work happens while coming back
-
-def fun(i):
-    if i == 0:
-        return 0
-    
-    return i + fun(i-1)
-
-n = int(input())
-print(fun(n))
-
-
-
-    
-
-
-
+main()
 
 
 
 
     
-
-
+    
 
 
 
