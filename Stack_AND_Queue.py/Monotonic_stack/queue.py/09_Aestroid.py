ebd41b3be30bc4b7539113aefe48fef7a727e603
veeresh-code24 <veereshkanamadi@gmail.com>
2026-08-18 23:18:01 +0530

    initial work

---
+++ Stack_AND_Queue.py/Monotonic_stack/queue.py/09_Aestroid.py
@@ -0,0 +1,31 @@
+def aestroidCollision(aestroid):
+    n = len(aestroid)
+    st = []
+    for i in range(n):
+        if aestroid[i] > 0:
+            st.append(aestroid[i])
+
+        else:
+            while st and st[-1] > 0 and st[-1] < abs(aestroid[i]):
+                st.pop()
+
+
+            if st and st[-1] == abs(aestroid[i]):
+                st.pop()
+
+
+            elif not st or st[-1] < 0:
+                st.append(aestroid[i])
+
+    return st
+
+
+
+
+    
+
+# aestroid = [3,5,-6,2,-1,4]
+# aestroid = [8,-8]
+# aestroid = [10,2,-5]
+aestroid = [3,5,-6,2,-1,4]
+print(aestroidCollision(aestroid))
